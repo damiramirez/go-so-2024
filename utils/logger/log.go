bf2b4d250f2e0ab75f552710c2a45c8a333dc3d0
Damian Ramirez <damianagustinrz@gmail.com>
2024-04-03 03:07:28 +0000

    Se agrego variable de ambiente y dependiendo esa variable logeo DEBUG o no

--- utils/logger/log.go
+++ utils/logger/log.go
@@ -18,10 +18,15 @@ const (
 type Logger struct {
 	Logger  *log.Logger
 	LogFile *os.File
+	environment string
 }
 
 // Log formats and logs a message at the given severity level.
 func (l *Logger) Log(message string, level LogLevel) {
+	if l.environment == "prod" && level == "DEBUG" {
+		return
+	}
+
 	format := fmt.Sprintf("[%s]: %s", level, message)
 	l.Logger.Println(format)
 }
@@ -31,12 +36,12 @@ func (l *Logger) CloseLogger() {
 }
 
 // Initializes a Logger to write to both stdout and a specified file.
-func ConfigureLogger(filename string) Logger {
+func ConfigureLogger(filename, env string) Logger {
 	logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 	if err != nil {
 		panic(err)
 	}
 	mw := io.MultiWriter(os.Stdout, logFile)
-	logger := Logger{Logger: log.New(mw, "", log.LstdFlags), LogFile: logFile}
+	logger := Logger{Logger: log.New(mw, "", log.LstdFlags), LogFile: logFile, environment: env}
 	return logger
 }
