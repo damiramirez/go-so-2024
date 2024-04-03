9f06741ad69321d24ccabc1b9bd73e7369ad8e8a
Damian Ramirez <damianagustinrz@gmail.com>
2024-04-03 02:44:15 +0000

    Se agrego funcion para cargar la config con tipo generic

---
+++ utils/config/config.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+
+	utils "github.com/sisoputnfrba/tp-golang/utils/logger"
+)
+
+func LoadConfiguration[T any](filePath string, logger utils.Logger) T{
+	var config T
+	
+	configFile, err := os.Open(filePath)
+	if err != nil {
+		logger.Log(err.Error(), utils.ERROR)
+		os.Exit(1)
+	}
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+
+	logger.Log("Config created", utils.INFO)
+
+	return config
+}
