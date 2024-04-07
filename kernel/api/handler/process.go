33b9f86b7435572c60c4afe9e19eecefddc62cdb
Damian Ramirez <damianagustinrz@gmail.com>
2024-04-06 22:18:38 -0300

    Se movio los handler de planificacion a su file correspondiente

--- kernel/api/handler/process.go
+++ kernel/api/handler/process.go
@@ -83,28 +83,3 @@ func ListProcessHandler(w http.ResponseWriter, r *http.Request, logger log.Logge
 
 	serialization.EncodeHTTPResponse(w, processes, http.StatusOK)
 }
-
-// Este mensaje se encargará de retomar (en caso que se encuentre pausada)
-// la planificación de corto y largo plazo. En caso que la planificación no
-// se encuentre pausada, se debe ignorar el mensaje.
-func InitPlanningHandler(w http.ResponseWriter, r *http.Request, logger log.Logger) {
-	logger.Log("Init planificacion", log.DEBUG)
-
-	// TODO: Manejar planificacion
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNoContent)
-}
-
-// Este mensaje se encargará de pausar la planificación de corto y largo plazo.
-// El proceso que se encuentra en ejecución NO es desalojado, pero una vez que salga
-// de EXEC se va a pausar el manejo de su motivo de desalojo. De la misma forma,
-// los procesos bloqueados van a pausar su transición a la cola de Ready.
-func StopPlanningHandler(w http.ResponseWriter, r *http.Request, logger log.Logger) {
-	logger.Log("Stop planificacion", log.DEBUG)
-
-	// TODO: Frenar planificacion
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNoContent)
-}
