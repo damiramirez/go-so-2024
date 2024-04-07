64e2c1e9825f457fdfb8ab12a643fac54facfae4
Damian Ramirez <damianagustinrz@gmail.com>
2024-04-07 18:58:39 -0300

    make clean para eliminar los bins de los modulos

--- kernel/api/server.go
+++ kernel/api/server.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/sisoputnfrba/tp-golang/kernel/api/handler"
+	handler "github.com/sisoputnfrba/tp-golang/kernel/api/handler"
 	log "github.com/sisoputnfrba/tp-golang/utils/logger"
 )
 
