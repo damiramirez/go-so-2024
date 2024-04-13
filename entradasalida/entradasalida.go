4d44a91090451ab1f1b8e2eb3e2921a823260f35
juanluengo88 <jluengo@frba.utn.edu.ar>
2024-04-13 21:35:53 +0000

    agrego funcion DeleteHTTP al utils en brach connections

--- entradasalida/entradasalida.go
+++ entradasalida/entradasalida.go
@@ -18,18 +18,19 @@ type ProcessState struct {
 }
 
 func main() {
+	var process ProcessState
+	endpoint:="plani/"
 	args := os.Args[1:]
 	if len(args) != 1 {
 		fmt.Println("Uso: programa <go run `modulo`.go dev|prod>")
 		return
 	}
 	env := args[0]
-
 	logger := log.ConfigureLogger(IOLOG, env)
 	ioConfig := config.LoadConfiguration[global.IOConfig]("./config/config.json", logger)
 
-	processSlice, _ := requests.GetHTTP[ProcessState](ioConfig.IPKernel, ioConfig.PortKernel, "process/12", &logger)
-
+	//processSlice, _ := requests.GetHTTP[ProcessState](ioConfig.IPKernel, ioConfig.PortKernel, "process/12", &logger)
+	processSlice,_ := requests.DeleteHTTP[ProcessState](endpoint,logger,ioConfig.PortKernel,process,ioConfig.IPKernel)
 	logger.Log(fmt.Sprintf("%+v", processSlice), log.INFO)
 
 	logger.CloseLogger()
--- utils/requests/requests.go
+++ utils/requests/requests.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	log "github.com/sisoputnfrba/tp-golang/utils/logger"
 )
@@ -32,7 +33,6 @@ func GetHTTP[T any](ip string, port int, endpoint string, logger *log.Logger) (*
 
 func PutHTTPwithBody[T any, R any](ip string, port int, endpoint string, data T, logger *log.Logger) (*R, error) {
 	var RespData *R
-
 	url := fmt.Sprintf("http://%s:%d/%s", ip, port, endpoint)
 
 	logger.Log("POST to: "+url, log.DEBUG)
@@ -66,3 +66,49 @@ func PutHTTPwithBody[T any, R any](ip string, port int, endpoint string, data T,
 
 	return RespData, nil
 }
+//desarolle una funcion q permite hacer los deletes con plani y process con PID aplicable a la funcion de arriba
+//para q funcione deberiamos pasarles como parametro en donde dice endpointwithpid en el caso de un proccess/pid
+// en el caso de plani/  
+func DeleteHTTP[T any](endpointwithPID string,logger log.Logger,port int,data T,ip string) (*T,error){
+	var RespData T
+	Delimitador := "/"
+	SplitString := strings.Split(endpointwithPID, Delimitador)
+	endpoint:=SplitString[0]
+	var url string
+	Pid:=SplitString[1]
+	if len(Pid)==0 {
+		url = fmt.Sprintf("http://%s:%d/%s", ip, port, endpoint)
+	} 
+	if len(Pid)!=0{
+		url = fmt.Sprintf("http://%s:%d/%s/%s", ip, port, endpoint,Pid)
+	}
+	body, err := json.Marshal(data)
+
+	if err != nil {
+		logger.Log("Error al encodear la estructura: "+err.Error(), log.ERROR)
+		return &RespData, err
+	}
+	req, err := http.NewRequest(http.MethodDelete, url, bytes.NewBuffer(body))
+
+	if err != nil {
+		logger.Log("error al hacer el request: "+err.Error(), log.ERROR)
+		return &RespData, err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		logger.Log("Error al enviar la solicitud: "+err.Error(), log.ERROR)
+		return &RespData, err
+	}
+	defer resp.Body.Close()
+	_ = json.NewDecoder(resp.Body).Decode(&RespData)
+	//no devuelve nada por ahora por lo que despues se deberia modificar
+	/*if err != nil {
+		logger.Log(fmt.Sprintf("Error al decodificar la respuesta: %s", err), log.ERROR)
+		return &RespData, err
+	}*/ //comento para despues implementarlo en el caso de que no se pueda decodificar la respuesta 
+
+	return &RespData,nil
+}
+
+
