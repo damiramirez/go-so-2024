42ada96cadf7e0c8f0a1a50f2be5ef33771cda87
juanluengo88 <jluengo@frba.utn.edu.ar>
2024-04-15 16:42:18 +0000

    mesj

--- memoria/memoria.go
+++ memoria/memoria.go
@@ -24,9 +24,9 @@ func main() {
 
 	s := api.CreateServer()
 
-	global.Logger.Log(fmt.Sprintf("Starting kernel server on port: %d", global.MemoryConfig.Port), log.INFO)
+	global.Logger.Log(fmt.Sprintf("Starting Memory server on port: %d", global.MemoryConfig.Port), log.INFO)
 	if err := s.Start(); err != nil {
-		global.Logger.Log(fmt.Sprintf("Failed to start kernel server: %v", err), log.ERROR)
+		global.Logger.Log(fmt.Sprintf("Failed to start Memory server: %v", err), log.ERROR)
 		os.Exit(1)
 	}
 
