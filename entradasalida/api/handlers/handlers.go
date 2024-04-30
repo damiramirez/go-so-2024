6f7514c41adbda6efa6451a3dda2a918e3633787
Iñaki <iaristia@frba.utn.edu.ar>
2024-04-30 16:46:24 -0300

    reacomodo función Sleep

--- entradasalida/api/handlers/handlers.go
+++ entradasalida/api/handlers/handlers.go
@@ -29,20 +29,22 @@ func Sleep(w http.ResponseWriter, r *http.Request) {
 		global.Logger.Log("Error al decodear: "+err.Error(), log.ERROR)
 		http.Error(w, "Error al decodear", http.StatusBadRequest)
 	}
-	global.Logger.Log(fmt.Sprintf("%+v", estructura), log.INFO)
+	global.Logger.Log(fmt.Sprintf("Me llegó ésta instrucción: %+v", estructura), log.INFO)
 	
 	global.Logger.Log(fmt.Sprintf("%+v", global.MapIOGenericsActivos[estructura.Nombre]), log.INFO)
 
 	dispositivo := global.MapIOGenericsActivos[estructura.Nombre]
 
-	dispositivo.EstaEnUso = true
+	// if es un IO existente y Generic -> ejecuto el sleep, sino -> "IO intexistente / este tipo de IO no duerme" [este chequeo lo hace Kernel o Entradasalida?]
 
-	global.Logger.Log(fmt.Sprintf("%+v", global.MapIOGenericsActivos), log.INFO)
+	global.Logger.Log(fmt.Sprintf("a punto de dormir: %+v", global.MapIOGenericsActivos[estructura.Nombre]), log.INFO)
 
-	global.Logger.Log("a punto de dormir", log.INFO)
+	dispositivo.EstaEnUso = true // fix to do: no se ve reflejado en el log
+
+	global.Logger.Log(fmt.Sprintf("durmiendo: %+v", global.MapIOGenericsActivos[estructura.Nombre]), log.INFO)
 
 	time.Sleep(time.Duration(estructura.Tiempo * global.IOConfig.UnitWorkTime)*time.Millisecond)
 
-	global.Logger.Log("terminé de dormir", log.INFO)
+	global.Logger.Log(fmt.Sprintf("terminé de dormir: %+v", global.MapIOGenericsActivos[estructura.Nombre]), log.INFO)
 
 }
